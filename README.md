69aa0f5e3ee7b82f636445e3c70cfd0c78c5a04d
zatyamin <zatyamin@yahoo.com>
2024-07-06 10:14:11 +0300

    Add method for sending requests

--- README.md
+++ README.md
@@ -7,6 +7,29 @@ In the project directory, you can run:
 - `npm start` - runs the app in the development mode
 - `npm run build` - builds the app for production to the `build` folder
 
+In development mode, specify the server address in the env variable `PROXY`:
+
+```
+PROXY=http://localhost:4000
+```
+
+In production mode, configure proxying of `/api/..` requests to the server address. Nginx example:
+
+```
+server {
+  listen 80;
+
+  location / {
+    root   html;
+    index  index.html;
+  }
+
+  location /api {
+    proxy_pass http://localhost:4000/api;
+  }
+}
+```
+
 ## License
 
 [MIT](./LICENSE)
---
+++ src/services/index.ts
@@ -0,0 +1,60 @@
+export enum RequestMethod {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+}
+
+export enum ResponseFormat {
+  TEXT = "text",
+  JSON = "json",
+  FORM_DATA = "formData",
+  BLOB = "blob",
+  ARRAY_BUFFER = "arrayBuffer",
+}
+
+export interface RequestParams {
+  url: string;
+  method?: RequestMethod;
+  query?: { [key: string]: any };
+  body?: { [key: string]: any };
+  format?: ResponseFormat;
+  keepalive?: boolean;
+}
+
+export interface ResponseError {
+  status: number;
+  code?: string;
+  text?: string;
+}
+
+export const sendRequest = async ({ url, method, body, query, format, keepalive }: RequestParams) => {
+  const base = process.env.NODE_ENV === "development" ? process.env.PROXY : window.location.origin;
+  const objectUrl = new URL(url, base);
+
+  for (const key in query) {
+    objectUrl.searchParams.append(key, query[key]);
+  }
+
+  const init: RequestInit = {
+    method: method || RequestMethod.GET,
+    keepalive,
+    credentials: "include",
+  };
+
+  if (method === RequestMethod.POST || method === RequestMethod.PUT) {
+    init.headers = { "Content-Type": "application/json" };
+    init.body = JSON.stringify(body || "");
+  }
+
+  const response = await fetch(objectUrl, init);
+
+  if (response.ok) {
+    return await response[format || ResponseFormat.JSON]();
+  }
+
+  const { status } = response;
+  const { code, text } = await response.json();
+
+  return Promise.reject({ status, code, text });
+};
