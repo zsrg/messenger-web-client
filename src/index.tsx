c3debb834a8b149202129f7b9636b5abf51e2cfc
zatyamin <zatyamin@yahoo.com>
2024-07-06 09:25:56 +0300

    Add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import store from "./redux";
+import { Provider } from "react-redux";
 import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
