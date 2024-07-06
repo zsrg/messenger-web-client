5163ce0f4044b5c95eaccf9b7f488805baa96c2c
zatyamin <zatyamin@yahoo.com>
2024-07-06 10:14:50 +0300

    Add i18n

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import store from "./redux";
 import { Provider } from "react-redux";
+import "./i18n";
 import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
